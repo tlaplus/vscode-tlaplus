d5da147fa1c94845f59e5f3e6745c44d9050958b
Andrew Lygin <alygin@egartech.com>
2019-07-16 19:28:09 +0300

    Check model command implementation, closes #5

--- src/cmd-check-model.ts
+++ src/cmd-check-model.ts
@@ -1,6 +1,9 @@
 import * as vscode from 'vscode';
 import path = require('path');
 import * as tools from './tla2tools';
+import {TYPE as TASK_TYPE, SOURCE as TASK_SOURCE} from './tasks';
+
+const tasks = new Map<string, vscode.Task>();
 
 /**
  * Runs TLC on a TLA+ specification.
@@ -18,17 +21,56 @@ export function checkModel(diagnostic: vscode.DiagnosticCollection) {
     doCheckModel(editor.document.uri, diagnostic);
 }
 
-async function doCheckModel(fileUri: vscode.Uri, diagnostic: vscode.DiagnosticCollection) {
-    const javaPath = tools.buildJavaPath();
-    if (!javaPath) {
+function doCheckModel(fileUri: vscode.Uri, diagnostic: vscode.DiagnosticCollection) {
+    const task = getTask(fileUri.fsPath);
+    if (!task) {
         return;
     }
-    const toolsArgs = ['-XX:+UseParallelGC', '-jar', tools.toolsJarPath, fileUri.fsPath];
-    let workDir = path.dirname(fileUri.fsPath);
-    return tools.executeJavaProcess(javaPath, toolsArgs, workDir)
-            .then(res => {
-                console.log('Check model ERR: ' + res.err);
-                console.log('---- Check model STDOUT ----\n' + res.stdout);
-                console.log('---- Check model STDERR ----\n' + res.stderr);            
-            });
+    const exec = vscode.tasks.executeTask(task);
+}
+
+function getTask(filePath: string): vscode.Task | undefined {
+    let task: vscode.Task | undefined = tasks.get(filePath);
+    if (!task) {
+        task = createTask(filePath);
+        if (task) {
+            tasks.set(filePath, task);
+        }
+    }
+    return task;
+}
+
+/**
+ * Creates task for running model checking process.
+ */
+function createTask(filePath: string): vscode.Task | undefined {
+    let workDir = path.dirname(filePath);
+    const javaPath = tools.buildJavaPath();
+    if (!javaPath) {
+        return undefined;
+    }
+    const opt: vscode.ProcessExecutionOptions = {
+        cwd: workDir
+    };
+    const taskDef: vscode.TaskDefinition = {
+        type: TASK_TYPE,
+        filePath: filePath
+    };
+    const execParts = [];
+    execParts.push('"' + javaPath + '"');
+    execParts.push('-XX:+UseParallelGC');
+    execParts.push('-jar');
+    execParts.push(tools.toolsJarPath);
+    execParts.push('"' + filePath + '"');
+    const exec = new vscode.ShellExecution(execParts.join(' '), opt);
+    const task = new vscode.Task(taskDef, 'Check model', TASK_SOURCE, exec);
+    //task.problemMatchers = [{'$tla-pluscal-transpiler'}];
+    task.presentationOptions = {
+        clear: true,
+        echo: false,
+        focus: false,
+        panel: vscode.TaskPanelKind.Shared,
+        reveal: vscode.TaskRevealKind.Always
+    };
+    return task;
 }
--- src/tasks.ts
+++ src/tasks.ts
@@ -1,5 +1,3 @@
-import * as vscode from 'vscode';
-
 /**
  * Value of Task.source that must be used in all TLA+ tasks.
  */
@@ -9,10 +7,3 @@ export const SOURCE = 'tla';
  * Value of TaskDefinition.type that must be used in all TLA+ tasks.
  */
 export const TYPE = 'tla';
-
-/**
- * Interface of tasks on TLA+ modules.
- */
-export interface TLATaskDefinition extends vscode.TaskDefinition {
-    readonly filePath: string;
-}
