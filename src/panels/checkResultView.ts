175b1c7ae18ff2f39447d23e09e5818b1b69cea7
Markus Alexander Kuppe <github.com@lemmster.de>
2024-01-08 13:57:34 -0800

    Support ANSI escape sequence in trace explorer web view.
    ansi-to-react does not need `unsafe-inline`.
    
    Corrects merge mistake of Github issue #320
    https://github.com/tlaplus/vscode-tlaplus/issues/320
    
    [Feature]
    
    Signed-off-by: Markus Alexander Kuppe <github.com@lemmster.de>

--- src/panels/checkResultView.ts
+++ src/panels/checkResultView.ts
@@ -145,7 +145,7 @@ class CheckResultViewPanel {
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'unsafe-inline' ${webview.cspSource}; font-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; font-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
                 <link rel="stylesheet" href="${styleUri}">
                 <title>Model checking</title>
             </head>
