c7a9526581b31b0a4fcbd4dbf853d71ee0aa5c62
Karolis Petrauskas <k.petrauskas@gmail.com>
2025-05-03 14:57:53 +0300

    Use distinct icons for note level.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- src/webview/current-proof-step/noteIcon/index.tsx
+++ src/webview/current-proof-step/noteIcon/index.tsx
@@ -5,7 +5,7 @@ interface NoteIconI { level: string }
 export const NoteIcon = React.memo(({ level }: NoteIconI) => {
     return (
         <span
-            className={`codicon codicon-info note-icon note-icon-${level}`}
+            className={`codicon codicon-${level} note-icon note-icon-${level}`}
             title={level}
         ></span>
     );
