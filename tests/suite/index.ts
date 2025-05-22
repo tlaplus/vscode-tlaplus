1b23884b39091e6e00e2218dde0caa3a1a326497
Markus Alexander Kuppe <github.com@lemmster.de>
2025-05-21 19:32:49 -0700

    Fix Mocha TypeScript errors that appeared out of nowhere.
    [Tests]
    
    Signed-off-by: Markus Alexander Kuppe <github.com@lemmster.de>

--- tests/suite/index.ts
+++ tests/suite/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import Mocha from 'mocha';
+import glob from 'glob';
 
 export function run(): Promise<void> {
     const mocha = new Mocha({
@@ -9,14 +9,14 @@ export function run(): Promise<void> {
     });
     const testsRoot = path.resolve(__dirname, '..');
     return new Promise((resolve, reject) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+        glob('**/**.test.js', { cwd: testsRoot }, (err: Error | null, files: string[]) => {
             if (err) {
                 return reject(err);
             }
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+            files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
             try {
                 // Run the mocha test
-                mocha.run(failures => {
+                mocha.run((failures: number) => {
                     if (failures > 0) {
                         reject(new Error(`${failures} tests failed.`));
                     } else {
