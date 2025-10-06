70fbc2f2273ef7b8327be1b3dba14d949ccaf104
Y <git@younes.io>
2025-10-06 15:18:34 +0000

    Harden validateWorkspacePath against symlink escapes
    Signed-off-by: Y <git@younes.io>

--- tests/suite/index.ts
+++ tests/suite/index.ts
@@ -7,6 +7,10 @@ export function run(): Promise<void> {
         ui: 'tdd',
         color: true
     });
+    const grep = process.env.MOCHA_GREP;
+    if (grep) {
+        mocha.grep(grep);
+    }
     const testsRoot = path.resolve(__dirname, '..');
     return new Promise((resolve, reject) => {
         glob('**/**.test.js', { cwd: testsRoot }, (err: Error | null, files: string[]) => {
