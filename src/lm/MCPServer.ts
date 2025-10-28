9146cc59a19b8401f5e45f626c395c59d080ebd1
Federico Ponzi <me@fponzi.me>
2025-10-28 21:22:52 +0000

    Reapply "Merge stdout/stderr streams and add a test to ensure we are always ab…"
    This reverts commit 1906b6ec43bbb932c3401a83a210ac26a8db7c38.
    
    Signed-off-by: Federico Ponzi <me@fponzi.me>

--- src/lm/MCPServer.ts
+++ src/lm/MCPServer.ts
@@ -1111,9 +1111,8 @@ export class MCPServer implements vscode.Disposable {
                     }
                 };
 
-                // Add event listeners to capture stdout and stderr
-                procInfo.process.stdout?.on('data', stdoutHandler);
-                procInfo.process.stderr?.on('data', stdoutHandler);
+                // Add event listener to capture merged output
+                procInfo.mergedOutput.on('data', stdoutHandler);
 
                 // Listen for process completion
                 procInfo.process.on('close', (code) => {
