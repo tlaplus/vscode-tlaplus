31c63b4839db3062db6ebe466414cd1eada62c37
Y <git@younes.io>
2025-11-02 09:21:35 +0100

    fix lint issues
    Signed-off-by: Y <git@younes.io>

--- tests/playwright/support/server.ts
+++ tests/playwright/support/server.ts
@@ -14,50 +14,50 @@ export interface FixtureServer {
 }
 
 export async function startFixtureServer(): Promise<FixtureServer> {
-	const port = await getAvailablePort();
-	const host = '127.0.0.1';
-	const app = express();
+    const port = await getAvailablePort();
+    const host = '127.0.0.1';
+    const app = express();
 
-	app.use('/static', express.static(OUT_ROOT));
-	app.use('/data', express.static(DATA_ROOT));
-	app.use('/', express.static(PUBLIC_ROOT, { extensions: ['html'] }));
+    app.use('/static', express.static(OUT_ROOT));
+    app.use('/data', express.static(DATA_ROOT));
+    app.use('/', express.static(PUBLIC_ROOT, { extensions: ['html'] }));
 
-	const server = await listenAsync(app, port, host);
-	return {
-		endpoint: `http://${host}:${port}/`,
-		dispose: () =>
-			new Promise((resolve, reject) => {
-				server.close(err => {
-					if (err) {
-						reject(err);
-					} else {
-						resolve();
-					}
-				});
-			})
-	};
+    const server = await listenAsync(app, port, host);
+    return {
+        endpoint: `http://${host}:${port}/`,
+        dispose: () =>
+            new Promise((resolve, reject) => {
+                server.close(err => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            })
+    };
 }
 
 function listenAsync(app: express.Express, port: number, host: string): Promise<http.Server> {
-	return new Promise(resolve => {
-		const server = app.listen(port, host, () => resolve(server));
-	});
+    return new Promise(resolve => {
+        const server = app.listen(port, host, () => resolve(server));
+    });
 }
 
 async function getAvailablePort(): Promise<number> {
-	return await new Promise((resolve, reject) => {
-		const server = net.createServer();
-		server.unref();
-		server.on('error', reject);
-		server.listen(0, '127.0.0.1', () => {
-			const address = server.address() as AddressInfo;
-			server.close(err => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(address.port);
-				}
-			});
-		});
-	});
+    return await new Promise((resolve, reject) => {
+        const server = net.createServer();
+        server.unref();
+        server.on('error', reject);
+        server.listen(0, '127.0.0.1', () => {
+            const address = server.address() as AddressInfo;
+            server.close(err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(address.port);
+                }
+            });
+        });
+    });
 }
